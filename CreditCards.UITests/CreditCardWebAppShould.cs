7f8ea8ab70a1916bac9e40ea357ab01d4a3167bd
Kratika Chandra <krchandr@microsoft.com>
2022-09-16 12:59:26 +0530

    Fix UI Test discoverability

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -1,18 +1,18 @@
-﻿namespace CreditCards.UITests
-{
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using OpenQA.Selenium;
-    using OpenQA.Selenium.Chrome;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
+namespace CreditCards.UITests
+{
     [TestClass]
-    class CreditCardWebAppShould
+    public class CreditCardWebAppShould
     {
         [TestMethod]
         public void LoadApplicationPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-
+                driver.Navigate().GoToUrl("http://localhost:5258/");
             }
         }
     }
