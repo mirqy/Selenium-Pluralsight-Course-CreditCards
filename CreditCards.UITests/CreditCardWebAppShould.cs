c6222b356b4ddb4ab96c8c6cfeff0502f0f5fb74
Kratika Chandra <krchandr@microsoft.com>
2022-09-16 12:33:52 +0530

    Add UI Test Project

---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,19 @@
+﻿namespace CreditCards.UITests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    [TestClass]
+    class CreditCardWebAppShould
+    {
+        [TestMethod]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+            }
+        }
+    }
+}
