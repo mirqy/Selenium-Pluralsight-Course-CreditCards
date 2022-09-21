7d3eb2fa1d17105251b5795850416516a29c5fe8
Kratika Chandra <krchandr@microsoft.com>
2022-09-21 12:38:15 +0530

    Use javascript in tests

---
+++ CreditCards.UITests/JavascriptExamples.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.UITests
+{
+    [TestClass]
+    public class JavascriptExamples
+    {
+        [TestMethod]
+        public void ClickOverlayedLink()
+        {
+            using(IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/jsoverlay.html");
+                DemoHelper.Pause();
+                string script = "document.getElementById('HiddenLink').click()";
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript(script);
+                Assert.AreEqual("https://www.pluralsight.com/", driver.Url);
+            }
+        }
+
+        [TestMethod]
+        public void GetOverlayedLinkText()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:44108/jsoverlay.html");
+                DemoHelper.Pause();
+                string script = "return document.getElementById('HiddenLink').innerHTML;";
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                string linkText = (string)js.ExecuteScript(script);
+                Assert.AreEqual("Go to Pluralsight", linkText);
+            }
+        }
+    }
+}
